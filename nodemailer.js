2c9116932091d8a6cdaef816766bfe2865e3a101
ujjwal sahu <ujjwals6901@gmail.com>
2025-09-10 00:25:13 +0530

    nodemailer

---
+++ nodemailer.js
@@ -0,0 +1,18 @@
+const mailer = require("nodemailer")
+
+const transport = mailer.createTransport({
+    service:"gmail",
+    auth:{
+        user:process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD
+    }
+})
+const sendMail = async(to,subject,message)=>{
+   return transport.sendMail({
+    from:`"chal hat"<${process.env.Email_user}>`,
+    to ,
+    subject,
+    html:message
+   })
+};
+module.exports = sendMail
