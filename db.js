44149df12977a8279abc2ff2b79cf77c85af4e65
ujjwal sahu <ujjwals6901@gmail.com>
2025-09-15 21:20:09 +0530

    database connection

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
- const mongooseUrl = process.env.MONGO_LOCAL_URL
- //const mongooseAtlasUrl = process.env.MONGO_ATLAS_URL
+ //const mongooseUrl = process.env.MONGO_LOCAL_URL
+ const mongooseAtlasUrl = process.env.MONGO_ATLAS_URL
 
- mongoose.connect(mongooseUrl,{
+ mongoose.connect(mongooseAtlasUrl,{
     useNewUrlParser :true,
     useUnifiedTopology:true
  })
