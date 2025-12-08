34f6a96d41096fda26db43548e76ea218be18be7
ujjwal sahu <ujjwals6901@gmail.com>
2025-12-08 21:28:01 +0530

    Main server file

--- server.js
+++ server.js
@@ -7,14 +7,16 @@ const bodyParser = require('body-parser');
 const passport=require('./auth')
 const Ratelimiter =require('./ratelimiter')
 const helmet = require("helmet")
+const path = require("path")
 const cors = require('cors')
-
+const frontend = path.join(__dirname, "frontend");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(Ratelimiter())
 app.use(helmet())
 app.use(express.json())
 app.use(cors())
+app.use(express.static(path.join(frontend)));
 
 
 app.use(bodyParser.json())
@@ -28,6 +30,10 @@ const noteRoutes = require('./routes/noteRoutes')
 app.use('/note',noteRoutes)
 const userRoutes = require('./routes/userRoutes')
 app.use('/user',userRoutes)
+
+app.get((req, res) => {
+  res.sendFile(path.join(frontend, "login.html"));
+});
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT,()=>{
