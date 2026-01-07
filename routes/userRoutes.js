abf05b201ee9c384b558437634809e7623114374
ujjwal sahu <ujjwals6901@gmail.com>
2026-01-08 00:02:43 +0530

    user Routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -134,11 +134,7 @@ const transport = mailer.createTransport({
     auth:{
         user:process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
-    }, 
-    pool: true,             
-    maxConnections: 5,      
-    rateDelta: 1000,         
-    rateLimit: 5 
+    }
 });
 router.post('/signup-otp',validate(signup), async(req,res)=>{
     try{
